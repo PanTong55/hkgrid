b4b34c4e4bf7546abba51537a9b35c47f1b68b31
PanTong55 <pantong0505@gmail.com>
2025-05-24 17:31:40 +0800

    Update geolocation.js

--- js/geolocation.js
+++ js/geolocation.js
@@ -220,19 +220,13 @@ export function initLocateTool(map, buttonId) {
             icon: L.divIcon({
               className: "lucide-locate-icon",
               html: `
-                <div class="rotate-container" style="
-                  width: 28px;
-                  height: 28px;
-                  border: 2px solid white;
-                  border-radius: 50%;
-                  background: white;
-                  display: flex;
-                  align-items: center;
-                  justify-content: center;
-                  box-sizing: border-box;
-                ">
-                  <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24">
-                    <path d="M12 2l6 20-6-4-6 4 6-20z" fill="#007aff" stroke="#007aff" stroke-width="1.5"/>
+                <div class="rotate-container" style="width: 24px; height: 24px;">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                    <path d="M12 2l6 20-6-4-6 4 6-20z"
+                          fill="#007aff"
+                          stroke="white"
+                          stroke-width="2"
+                          stroke-linejoin="round" />
                   </svg>
                 </div>
               `,
